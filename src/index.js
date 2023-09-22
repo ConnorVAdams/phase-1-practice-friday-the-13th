7b7e896981c8c08c56f409dd11cb174c80bbb2f7
Connor Adams <connorvadams@gmail.com>
2023-09-21 19:35:34 -0600

    Defined general CRUD functions.

---
+++ src/index.js
@@ -0,0 +1,59 @@
+// ! Populate nav bar with all movies on page load
+
+// ! Load first movie in list to window on page load
+
+// ! Populate window with relevant info when a movie in nav bar is clicked
+
+// ! Button content changes from watched/unwatched
+
+// ! Persist information about watched/unwatched
+
+// ! Update number of blood drops when users adds to associated form
+
+// ! Persist information about blood drops
+
+// ! Variables
+const MOVIESURL = 'http://localhost:3000/movies';
+
+const movieListNav = document.querySelector('#movie-list');
+
+const movieInfoDiv = document.querySelector('#movie-info');
+    const movieInfoImg = document.querySelector('#detail-image');
+    const movieInfoTitle = document.querySelector('#title');
+    const movieInfoYear = document.querySelector('#year-released');
+    const movieInfoDesc = document.querySelector('#description');
+    const watchedBtn = document.querySelector('#watched');
+    const bloodAmount = document.querySelector('#amount');
+
+const bloodForm = document.querySelector('#form');
+    const amountInput = document.querySelector('#blood-amount');
+    const addBloodBtn = document.querySelector('form:nth-child(3)');
+
+// ! CRUD functions
+//GET all moviesObj to populate nav bar on page load
+const fetchAllMovies = () => {
+    return fetch(MOVIESURL)
+    .then(resp => resp.json())
+};
+
+//GET a movieObj to populate the window
+//Fire on first movie on page load 
+const fetchOneMovie = (selectedMovieId) => {
+    return fetch(`${MOVIESURL}/${selectedMovieId}`)
+    .then(resp => resp.json())
+};
+
+//PATCH blood_amount and watched depending on which value is passed in
+const patchOneMovie = (selectedMovieId, propertyToBePatched, newValue) => {
+    const patchData = {};
+    patchData[propertyToBePatched] = newValue;
+    fetch(`${MOVIESURL}/${selectedMovieId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(patchData)
+    })
+    .then(resp => resp.json())
+};
+
